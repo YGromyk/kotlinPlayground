0b0956bb8f3e6891393a27b969c24eb185d5fa1b
Yuriy Gromyk <yurii.gromyk@magnise.com>
2018-10-13 00:54:43 +0300

    Added Builders, Inheritance.

---
+++ src/kotlin/newfeatures/Builders.kt
@@ -0,0 +1,50 @@
+package kotlin.newfeatures
+
+@DslMarker
+annotation class ContextWarning
+
+@ContextWarning
+data class Person(
+        var name: String? = null,
+        var age: UInt? = null,
+        var contacts: Contacts? = null
+) {
+    companion object
+}
+
+@ContextWarning
+data class Contacts(var phoneNumber: String? = null, var email: String? = null)
+
+fun Person.Companion.create(block: Person.() -> Unit) = Person().apply(block)
+fun Person.contacts(build: Contacts.() -> Unit) {
+    contacts = Contacts().apply(build)
+}
+
+/**
+ * todo: functions that above are called
+ * 'is a function type with receiver'.
+ * This means that we need to pass an instance of type kotlin.newfeatures.Person(a receiver)
+ * to the function, and we can call members of that instance inside the function.
+ * The receiver can be accessed through the this keyword:'
+ *
+ **/
+
+fun main(args: Array<String>) {
+    print(Person.create {
+        name = "some name"
+        age = 18u
+        contacts {
+            /*
+                        name = "name"
+             todo: this thing is forbidden, 'cause we do use DSL marker
+             that give us the context of closest receiver
+            */
+            email = "someemail@gmail.com"
+            phoneNumber = "+3869852145"
+        }
+    })
+}
+
+data class A(var a: Int = 0)
+
+fun applyA(block: A.() -> Unit) = A().apply(block)
---
+++ src/mainjava/JavaMain.java
@@ -0,0 +1,9 @@
+package mainjava;
+
+import kotlin.sandbox.ChildB;
+
+public class JavaMain {
+    public static void main(String[] args) {
+        new ChildB();
+    }
+}
